52cfe6d9fde33763efb89ef51adceacbfd25d8b0
zhaoliping <tokisakiyuu@qq.com>
2020-08-29 16:26:48 +0800

    update entry

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-let ui = require('./ui');
+const ui = require("./ui");
 
-let current = 0;
-let arr = [];
-
-let pickerScreen = new ui.Screen("pickerScreen");
-
-pickerScreen
-    .lines(arr, (elem, index) => {
-        return (current === index ? "->" : ui.space(4)) + elem
-    })
-    ._("请选择一个:")
-
-pickerScreen.on("kepress:return", () => {});
+module.exports = ui;
